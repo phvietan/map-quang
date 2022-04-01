15e356b6cb48337e86c307e892f16d000b6ba5f2
drchain <phvietan@gmail.com>
2022-04-02 03:17:05 +0800

    done zoom, TODO: refactor

---
+++ js/global.js
@@ -0,0 +1,7 @@
+var global = {
+  windowWidth: window.innerWidth,
+  windowHeight: window.innerHeight,
+
+  sizeMapWidth: window.innerWidth * 85 / 100,
+  sizeMapHeight: window.innerHeight * 85 / 100,
+};
